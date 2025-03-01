b6aa3125f18307a33a588eaa022384e194c694f2
jahidhasansabbir1 <jahidhasansabbir1@gmail.com>
2025-03-01 12:29:06 +0600

    rename the blogs.html

--- script/script.js
+++ script/script.js
@@ -1,3 +1,3 @@
 document.getElementById('discover-section').addEventListener('click', function(){
-    window.location.href="dev-board/bolgs.html"
+    window.location.href="dev-board/blogs.html"
 })
